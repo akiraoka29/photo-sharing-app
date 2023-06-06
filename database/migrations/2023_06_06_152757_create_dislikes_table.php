71c4de0b4e5429e9160fd8ae6eada0863c8f5421
akiraoka29 <akiraoka.job@gmail.com>
2023-06-06 22:38:41 +0700

    Add New Database Migrations

---
+++ database/migrations/2023_06_06_152757_create_dislikes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDislikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dislikes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('photo_id')->constrained('photos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dislikes');
+    }
+}
---
+++ database/migrations/2023_06_06_153605_create_photo_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotoTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('photo_id')->constrained('photos');
+            $table->foreignId('tag_id')->constrained('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_tag');
+    }
+}
