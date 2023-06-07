0a45d3d8b97a16ed5c58549b09e30a63f6eaa3e2
akiraoka29 <akiraoka.job@gmail.com>
2023-06-07 15:38:32 +0700

    Update Header & Add New Photo Component into Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <main id="main">
-        <x-hero title="Our Largest Free Photo Communities" description="Sharing your photos is a form of caring for our community." image="img/hero-bg-1.jpg" />
+        <x-hero 
+            title="Our Largest Free Photo Communities" 
+            description="Sharing your photos is a form of caring for our community." 
+            image="img/hero-bg-1.jpg" />
+        
+        <x-photo />
     </main>
+
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,32 +10,8 @@
         <!-- Nav Menu -->
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="index.html" class="active">Home</a></li>
-                <li><a href="index.html#about">About</a></li>
-                <li><a href="index.html#services">Services</a></li>
-                <li><a href="index.html#portfolio">Portfolio</a></li>
-                <li><a href="index.html#team">Team</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li class="dropdown has-dropdown"><a href="#"><span>Dropdown</span> <i
-                            class="bi bi-chevron-down"></i></a>
-                    <ul class="dd-box-shadow">
-                        <li><a href="#">Dropdown 1</a></li>
-                        <li class="dropdown has-dropdown"><a href="#"><span>Deep Dropdown</span> <i
-                                    class="bi bi-chevron-down"></i></a>
-                            <ul class="dd-box-shadow">
-                                <li><a href="#">Deep Dropdown 1</a></li>
-                                <li><a href="#">Deep Dropdown 2</a></li>
-                                <li><a href="#">Deep Dropdown 3</a></li>
-                                <li><a href="#">Deep Dropdown 4</a></li>
-                                <li><a href="#">Deep Dropdown 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Dropdown 2</a></li>
-                        <li><a href="#">Dropdown 3</a></li>
-                        <li><a href="#">Dropdown 4</a></li>
-                    </ul>
-                </li>
-                <li><a href="index.html#contact">Contact</a></li>
+                <li><a href="#hero" class="active">Home</a></li>
+                <li><a href="#photos">Free Photos</a></li>
             </ul>
 
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
