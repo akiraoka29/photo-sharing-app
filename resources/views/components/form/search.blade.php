217a107d63225f7477feaa2e4ac99ae90153b46f
akiraoka29 <akiraoka.job@gmail.com>
2023-06-07 19:31:15 +0700

    Add Form Search Component & Update Hero Component

---
+++ resources/views/components/form/search.blade.php
@@ -0,0 +1,4 @@
+<form action="#" class="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
+    <input type="text" class="form-control" id="searchPhoto" placeholder="Search free photos">
+    <input type="submit" class="btn btn-primary" value="Search">
+</form>
