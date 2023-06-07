d1b578775ad51a67ac64131e6f9755b30ec24168
akiraoka29 <akiraoka.job@gmail.com>
2023-06-07 19:08:17 +0700

    Add Signup View with Modal

---
+++ resources/views/components/form/signup.blade.php
@@ -0,0 +1,17 @@
+<form>
+    <div class="mb-3">
+        <label for="emailAddress" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text d-none"><span class="text-danger">*</span>Required.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password">
+        <div id="passwordHelp" class="form-text d-none"><span class="text-danger">*</span>Required.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Retype Password</label>
+        <input type="password" class="form-control" id="retypePassword">
+        <div id="retypePasswordHelp" class="form-text d-none"><span class="text-danger">*</span>Required.</div>
+    </div>
+</form>
