e842ef6af5137df6a3a9659fa541eb199fbb5536
akiraoka29 <akiraoka.job@gmail.com>
2023-06-08 15:05:17 +0700

    Update Photo Component

---
+++ resources/views/components/photo/photo.blade.php
@@ -0,0 +1,10 @@
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-app" style="cursor: pointer;">
+    <img src="{{ asset('img/masonry-portfolio/masonry-portfolio-1.jpg') }}" class="img-fluid" alt="">
+    <div class="portfolio-info">
+        <h4>App 1</h4>
+        <p>Lorem ipsum, dolor sit</p>
+        <a href="{{ asset('img/masonry-portfolio/masonry-portfolio-1.jpg') }}" title="App 1"
+            data-gallery="portfolio-gallery-app" data-tags="app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+        <a href="javascript:void(0)" title="Like" class="details-link like-button"><i class="bi bi-heart"></i></a>
+    </div>
+</div><!-- End Portfolio Item -->
---
+++ resources/views/components/photo/tag.blade.php
@@ -0,0 +1,6 @@
+<ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
+    <li data-filter="*" class="filter-active">All</li>
+    <li data-filter=".filter-app">App</li>
+    <li data-filter=".filter-product">Card</li>
+    <li data-filter=".filter-branding">Web</li>
+</ul><!-- End Portfolio Filters -->
