75845bb1f5dfd62723470de2d75c1a6b4ea27069
akiraoka29 <akiraoka.job@gmail.com>
2023-06-13 01:44:36 +0700

    Fix db:seed error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(TagSeeder::class);
-        $this->call(PhotoSeeder::class);
+        $this->call(TagSeeder::class);
+        // $this->call(PhotoSeeder::class);
     }
 }
